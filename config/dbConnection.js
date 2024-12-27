0e27af4ca95dcf1cc94c6efcca55af2d4cef83af
Ganesh Dandekar <ganesh15122002@gmail.com>
2024-12-27 12:24:51 +0530

    minor change

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,7 +7,7 @@ const connectionToDB = async () => {
     try {
 
         const { connection } = await mongoose.connect(
-            process.env.Mongo_URL, { useNewUrlParser: true }
+            process.env.MONGO_URL
         );
     
         if(connection) {
