15ae0800801c2dbb543adc4adbd0df31400eb2ab
Ganesh Dandekar <ganesh15122002@gmail.com>
2025-01-02 23:40:42 +0530

    moinor change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,7 +9,8 @@ import crypto from 'crypto';
 const cookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000, // cookie set for 7 days
     httpOnly: true,
-    secure: true
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'None'
 }
 
 
